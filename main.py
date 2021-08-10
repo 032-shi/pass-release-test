3f54f1eaa7d3b440393bc7c4da4a41eb810dd956
032-shi <0324omitsu@gmail.com>
2021-08-10 21:41:23 +0900

    [start]

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+# 日本語対応させる
 import zipfile
 
 def main():
   with zipfile.ZipFile("sampleFolder.zip","r") as zip_file:
+    try:
+      zip_file.extractall(path=".", pwd="3456".encode("u"))
+      print("zipを解凍できました")
+    except RuntimeError:
+      print("zipを開けませんでした")
