11c7752fd9d38e1229e34ced06ea3710a2febd29
032-shi <0324omitsu@gmail.com>
2021-08-11 18:03:50 +0900

    [add]数字の生成機能実装

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 # coding: UTF-8
 import zipfile
 
-def main():
+def open_zip():
   with zipfile.ZipFile("sampleFolder.zip","r") as zip_file:
     try:
-      zip_file.extractall(path=".", pwd="3456".encode("u"))
+      password = "1234"
+      zip_file.extractall(path=".unZipFolder", pwd=password.encode())
       print("zipを解凍できました")
-    except RuntimeError:
+    except Exception as e:
+      print(e)
       print("zipを開けませんでした")
+
+def main():
+  numbers = list(range(10))
+  print(numbers)
+  my_passwords = []
+
+  for a in numbers:
+    print(a)
+
+if __name__ == "__main__":
+  main()
