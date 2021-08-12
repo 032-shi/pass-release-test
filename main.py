4e71d5376826d8093aa92d96f0bd7654a452b999
032-shi <0324omitsu@gmail.com>
2021-08-12 22:01:10 +0900

    [add]4桁の数字生成機能実装

--- main.py
+++ main.py
@@ -19,8 +19,12 @@ def main():
   for a in numbers: #1桁目の数字の生成
     #print(a)
     for b in numbers: #2桁目の数字の生成
-      answer = str(a) + str(b)
-      print(answer)
+      #answer = str(a) + str(b)
+      #print(answer)
+      for c in numbers:
+        for d in numbers:
+          prediction_password = str(a) + str(b) + str(c) + str(d)
+          print(prediction_password)
 
 if __name__ == "__main__":
   main()
