b1dba0471082bbe1431ac6dffa38ee990486bba1
032-shi <0324omitsu@gmail.com>
2021-08-13 17:53:13 +0900

    [add]zipフォルダーのパスワード総当たり機能実装

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 # coding: UTF-8
 import zipfile
 
-def open_zip():
+def open_zip(password):
   with zipfile.ZipFile("sampleFolder.zip","r") as zip_file:
     try:
-      password = "1234"
-      zip_file.extractall(path=".unZipFolder", pwd=password.encode())
+      zip_file.extractall(path="./unZipFolder", pwd=password.encode()) #path=に解凍後のファイルを保存するフォルダーを指定する
       print("zipを解凍できました")
+      print("パスワード:{}".format(password))
+      exit()
     except Exception as e:
-      print(e)
-      print("zipを開けませんでした")
+      pass
+      #print(e)
+      #print("zipを開けませんでした")
 
 def main():
   numbers = list(range(10))
-  print(numbers)
+  #print(numbers)
   my_passwords = []
 
   for a in numbers: #1桁目の数字の生成
-    #print(a)
     for b in numbers: #2桁目の数字の生成
-      #answer = str(a) + str(b)
-      #print(answer)
       for c in numbers: #3桁目の数字の生成
         for d in numbers: #4桁目の数字の生成
           prediction_password = str(a) + str(b) + str(c) + str(d)
           print(prediction_password)
+          open_zip(password=prediction_password)
 
 if __name__ == "__main__":
   main()
